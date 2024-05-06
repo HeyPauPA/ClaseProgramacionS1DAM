a7a3d00963b9781a364ec04ef5491804628c551d
Alumno-PPA <alumno.368350@ies-azarquiel.es>
2024-05-06 10:51:12 +0200

    v2

---
+++ Repaso_Archivos/src/EjercicioExamenRepaso1/Combinacion.java
@@ -0,0 +1,28 @@
+package EjercicioExamenRepaso1;
+
+import java.util.ArrayList;
+
+public class Combinacion 
+{
+	//------- Atributos -------
+	private ArrayList<Integer> numeroCombinacion;
+
+	//------ Constructores ----
+	public Combinacion(ArrayList<Integer> numeroCombinacion) {
+		super();
+		this.numeroCombinacion = numeroCombinacion;
+	}
+
+
+	//-------- Getters/Setters --------
+	public ArrayList<Integer> getNumeroCombinacion() {
+		return numeroCombinacion;
+	}
+	
+	public void setNumeroCombinacion(ArrayList<Integer> numeroCombinacion) {
+		this.numeroCombinacion = numeroCombinacion;
+	}
+
+
+	
+}
